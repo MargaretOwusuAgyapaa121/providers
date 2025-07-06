6cc42785db47e1769a4d2e41aaef19756e8a2366
MargaretMensah <margarermensah123@gmail.com>
2025-07-05 23:33:09 -0700

    test server

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,10 @@ const app = require('./app.js');
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('✅ Backend is working successfully! 🚀');
+});
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
-// import app from './app.js';
-
-// const PORT = process.env.PORT || 5000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
